0403a92056eb17906efa045cdb1cb9cfdc431004
Oviedo581 <boviedobolanos@gmail.com>
2024-10-14 13:49:55 -0500

    Union del backend de gastos al front de gastos

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    devServer: {
-      port: 8080, // El puerto predeterminado de Vue, puedes cambiarlo si lo necesitas
-      open: true, // Abre el navegador automáticamente
-    },
-  };
-  
